8c3f5da0c5e08a30484c47eef8e60a3746433e47
leon <Leon21brandstaetter@gmx.de>
2026-01-18 14:16:04 +0100

    Cleanup: remove temporary debug scripts and log files

--- scripts/fix_localstorage.js
+++
@@ -1,5 +0,0 @@
-// Simple script to fix localStorage
-console.log("Please open the browser console and run this:");
-console.log("localStorage.setItem('activeThemeInfo', JSON.stringify({id: 29, name: 'Default Blue'}))");
-console.log("OR");
-console.log("localStorage.removeItem('activeThemeInfo');");
